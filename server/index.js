2681910aa46ec9f61981b84758649560777d4cbd
Yusei Ito <yuseikun@Outlook.jp>
2020-04-05 20:47:56 +0900

    Modify: Lambda friendly

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-//Process starts from here
+// Process starts from here
+function alert(v) {
+    console.log(v);
+}
+
+alert("hello");
+const a = 0;
+if (a == 1) {
+    console.log("Hello, world");
+}
+
+exports.handler = async(event) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
