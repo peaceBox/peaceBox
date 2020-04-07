9b0144864e0f1c8b34cac3cd0a97d71bdab06055
Yusei Ito <yuseikun@Outlook.jp>
2020-04-07 11:53:08 +0900

    Use sass

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,20 +1,32 @@
 <template>
-<div>
-  <v-row align="center" justify="center">
-    <v-col align="center" fluid>
+  <div style="margin-top:100px;">
+    <v-row v-if="loggedIn" align="center" justify="center">
       <h1>Hello</h1>
-      </v-col>
-    <v-col align="center" fluid>
-      <h1>Hola</h1>
-    </v-col>
-  </v-row>
-  <v-row align="center" justify="center">
-    <h1>Hello</h1>
-  </v-row>
-</div>
+    </v-row>
+
+    <div v-else>
+      <v-row  align="center" justify="center">
+        <p>peaceBoxはTwitterでログインしてのみご利用いただけます。</p>
+      </v-row>
+      <v-row align="center" justify="center" style="margin-top:30px;">
+        <v-btn large color="#4CBADD" style="color:#ffffff;">Twitterでログイン</v-btn>
+      </v-row>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
+  data:()=>{
+    return{loggedIn:false}
+  }
 }
 </script>
+
+<style>
+p{
+  size:20px;
+  font-weight: 600;
+}
+</style>
