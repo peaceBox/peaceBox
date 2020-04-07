228f9cb822860c990e2a9a329d60c433e0c61e54
Yusei Ito <yuseikun@Outlook.jp>
2020-04-07 11:27:50 +0900

    Render with nuxt

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -1,17 +1,16 @@
 <template>
   <v-app id="inspire">
-
     <navigator v-model="drawer" />
     <v-app-bar app color="#0D90BA" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>peaceBox</v-toolbar-title>
     </v-app-bar>
 
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <slot></slot>
-      </v-container>
-    </v-content>
+     <main class="mainContainer">
+        <v-container class="px-4 py-8">
+          <nuxt />
+        </v-container>
+      </main>
     <v-footer color="#0D90BA" app>
       <span class="white--text">&copy; 2019</span>
     </v-footer>
@@ -21,7 +20,6 @@
 <script>
 import navigator from "../components/navigator"
 export default {
-  name:"defaultLayout",
     data: () => ({
         drawer: false,
     }),
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,32 +1,20 @@
 <template>
-<defaultLayout>
-        <v-row align="center" justify="center">
-          <v-col align="center" fluid>
-         <h1>Hello</h1>
-         </v-col>
-           <v-col align="center" fluid>
-         <h1>Hola</h1>
-         </v-col>
-        </v-row>
-         <v-row align="center" justify="center">
-         <h1>Hello</h1>
-        </v-row>
-        
-</defaultLayout>
+<div>
+  <v-row align="center" justify="center">
+    <v-col align="center" fluid>
+      <h1>Hello</h1>
+      </v-col>
+    <v-col align="center" fluid>
+      <h1>Hola</h1>
+    </v-col>
+  </v-row>
+  <v-row align="center" justify="center">
+    <h1>Hello</h1>
+  </v-row>
+</div>
 </template>
 
 <script>
-
-import defaultLayout from "../layouts/default"
 export default {
-    props: {
-        source: String,
-    },
-    components:{
-      defaultLayout
-    },
-    data: () => ({
-        drawer: null,
-    }),
 }
 </script>
