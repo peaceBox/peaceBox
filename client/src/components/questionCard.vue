77fc451d14f22c272faeaf5c6e8921d45a811ac8
Yusei Ito <yuseikun@Outlook.jp>
2020-04-11 17:33:37 +0900

    marginをつけた

--- client/src/components/questionCard.vue
+++ client/src/components/questionCard.vue
@@ -1,29 +1,34 @@
 <template>
-    <v-card>
-        <v-card-actions>
-            <v-list-item class="grow">
-                <v-list-item-avatar color="grey darken-3">
-                    <v-img class="elevation-6" :src="questioner.icon"></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                    <v-list-item-title
-                        >{{ questioner.name }} ({{
-                            questioner.screenName
-                        }})</v-list-item-title
-                    >
-                </v-list-item-content>
-            </v-list-item>
-        </v-card-actions>
-        <v-card-text class="headline font-weight-bold">{{
-            question
-        }}</v-card-text>
+    <div style="margin: 10px;">
+        <v-card>
+            <v-card-actions>
+                <v-list-item class="grow">
+                    <v-list-item-avatar color="grey darken-3">
+                        <v-img
+                            class="elevation-6"
+                            :src="questioner.icon"
+                        ></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title
+                            >{{ questioner.name }} ({{
+                                questioner.screenName
+                            }})</v-list-item-title
+                        >
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card-actions>
+            <v-card-text class="headline font-weight-bold">{{
+                question
+            }}</v-card-text>
 
-        <v-divider></v-divider>
+            <v-divider></v-divider>
 
-        <v-card-text v-if="answer != ''" class="headline">
-            {{ answer }}
-        </v-card-text>
-    </v-card>
+            <v-card-text v-if="answer != ''" class="headline">
+                {{ answer }}
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
 <script>
 export default {
