23b0359bcbc24b180a84a53d36c78baddfb91a72
Yusei Ito <yuseikun@Outlook.jp>
2020-04-11 17:23:39 +0900

    質問カードを追加

--- client/src/components/inbox.vue
+++ client/src/components/inbox.vue
@@ -1,5 +1,18 @@
 <template>
-    <questionCard screenName="ItyuJ" />
+    <questionCard
+        screenName="ItyuJ"
+        question=" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud"
+        answer="あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+            またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い"
+        :questioner="{
+            screenName: 'ItyuJ',
+            name: 'Ysuei Ito',
+            icon:
+                'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+        }"
+    />
 </template>
 <script>
 import questionCard from '../components/questionCard';
--- client/src/components/questionCard.vue
+++ client/src/components/questionCard.vue
@@ -3,31 +3,30 @@
         <v-card-actions>
             <v-list-item class="grow">
                 <v-list-item-avatar color="grey darken-3">
-                    <v-img
-                        class="elevation-6"
-                        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-                    ></v-img>
+                    <v-img class="elevation-6" :src="questioner.icon"></v-img>
                 </v-list-item-avatar>
                 <v-list-item-content>
-                    <v-list-item-title>Yusei Ito</v-list-item-title>
+                    <v-list-item-title
+                        >{{ questioner.name }} ({{
+                            questioner.screenName
+                        }})</v-list-item-title
+                    >
                 </v-list-item-content>
             </v-list-item>
         </v-card-actions>
-        <v-card-text class="headline font-weight-bold">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud
-        </v-card-text>
+        <v-card-text class="headline font-weight-bold">{{
+            question
+        }}</v-card-text>
 
         <v-divider></v-divider>
-        <v-card-text class="headline">
-            あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
-            　またそのなかでいっしょになったたくさんのひとたち、ファゼーロとロザーロ、羊飼のミーロや、顔の赤いこどもたち、地主のテーモ、山猫博士のボーガント・デストゥパーゴなど、いまこの暗い
+
+        <v-card-text v-if="answer != ''" class="headline">
+            {{ answer }}
         </v-card-text>
     </v-card>
 </template>
 <script>
 export default {
-    params: ['screenName'],
+    props: ['screenName', 'question', 'answer', 'questioner'],
 };
 </script>
