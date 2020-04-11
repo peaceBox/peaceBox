c7819ae58cf59b17e54de8f6b5c79fb1c537177b
Yusei Ito <yuseikun@Outlook.jp>
2020-04-11 18:50:14 +0900

    textaraとボタンを実装

--- client/src/components/answerInput.vue
+++ client/src/components/answerInput.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+    <div style="width: 100%;">
+        <h4>まだこの質問に回答していません!</h4>
+        <v-textarea success placeholder="回答を入力" />
+        <v-btn style="width: 100%;" color="success">送信</v-btn>
+    </div>
+</template>
 <script>
 export default {};
 </script>
