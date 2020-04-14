f156d8b03379b318816efd46abcb6ac1d3ce1a0b
Yusei Ito <yuseikun@Outlook.jp>
2020-04-14 15:06:33 +0900

    metaタグを導入

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -2,12 +2,20 @@ module.exports = {
     srcDir: './src',
     head: {
         titleTemplate: '%s - PeaceBox',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+        ],
     },
     modules: ['@nuxtjs/vuetify', 'nuxt-webfontloader'],
     vuetify: {
         customVariables: ['./src/assets/variables.scss'],
         treeShake: true,
     },
+
     webfontloader: {
         google: {
             families: ['Acme'],
