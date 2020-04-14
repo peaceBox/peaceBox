d24a57fc351369aa8c3aee0cbd47346bcdb0894a
Yusei Ito <yuseikun@Outlook.jp>
2020-04-14 15:08:20 +0900

    ユーザページを追加
    domain.tld/username で特定ユーザのが使えるように。

---
+++ client/src/pages/_screenName.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="margin-top: 100px;">
+        <v-row v-if="loggedIn" align="center" justify="center">
+            <h1>Hello</h1>
+        </v-row>
+        <v-card>
+            <v-card-title></v-card-title>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return { loggedIn: false, isRegisterd: true };
+    },
+};
+</script>
+
+<style>
+p {
+    size: 20px;
+    font-weight: 600;
+}
+</style>
