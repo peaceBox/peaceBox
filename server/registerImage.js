d8fcadc865a5980ba98eefd4f5398778079d6410
shinbunbun <takamuneyuto@gmail.com>
2020-05-03 18:31:06 +0900

    fix registerImage

--- server/registerImage.js
+++ server/registerImage.js
@@ -4,30 +4,71 @@ AWS.config.update({
 });
 
 exports.registerImage = async (event) => {
+  const others = require('./others');
+
   const data = JSON.parse(event.body).data;
+  const userId = data.userId;
+
+  const isLoggedIn = await others.isLoggedIn(event, userId);
+  if (isLoggedIn === 'authorizationError') {
+    const response = {
+      statusCode: 401,
+      body: JSON.stringify('Authorization Error!!'),
+      headers: {
+        'Access-Control-Allow-Origin': 'https://peacebox.shinbunbun.info',
+        'Access-Control-Allow-Credentials': true
+      }
+    };
+    return response;
+  } else if (isLoggedIn === 'expired') {
+    const response = {
+      statusCode: 302,
+      headers: {
+        'Location': 'https://api.peacebox.shinbunbun.info/authorize?type=logIn',
+        'Access-Control-Allow-Origin': 'https://peacebox.shinbunbun.info',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: ''
+    };
+    return response;
+  }
+
+  console.log(data);
   const questionId = data.questionId;
   const image = data.image.replace(/^data:\w+\/\w+;base64,/, '');
-  const decodedImage = new Buffer(image, 'base64');
+  const decodedImage = Buffer.from(image, 'base64');
 
   const params = {
     Body: decodedImage,
     Bucket: 'peacebox-images',
     Key: [questionId, 'jpeg'].join('.'),
-    ContentType: 'image/jpeg'
+    ContentType: 'image/jpeg',
+    ACL: 'public-read'
   };
 
   const s3 = new AWS.S3();
-  await s3.putObject(params).promise()
-    .then(() => {
-      console.log('Success!!!');
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
+  await new Promise((resolve, reject) => {
+    s3.upload(params, function (err, data) {
+      if (err) {
+        console.log('error : ', err);
+        reject(err);
+      } else {
+        console.log('success');
+        resolve();
+      }
     });
+  });
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify('success')
+    headers: {
+      'Access-Control-Allow-Origin': 'https://peacebox.shinbunbun.info',
+      'Access-Control-Allow-Credentials': true
+      // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
+    body: JSON.stringify({
+      imageUrl: `https://peacebox-images.s3-ap-northeast-1.amazonaws.com/${questionId}.jpeg`
+    })
   };
   return response;
 };
