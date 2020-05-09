a6707b1279abe8b4be4479276d8b74a126591eac
Yusei Ito <yuseikun@Outlook.jp>
2020-05-10 00:14:20 +0900

    Cookie handling

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -5,47 +5,45 @@
         </div>
         <div v-else>
             <v-row align="center" justify="center">
-                <p>
-                    peaceBoxでの質問や回答はTwitterでログインしてのみご利用いただけます。
-                </p>
+                <p>peaceBoxでの質問や回答はTwitterでログインしてのみご利用いただけます</p>
             </v-row>
             <v-row align="center" justify="center" style="margin-top: 30px;">
                 <v-btn
                     large
                     color="#4CBADD"
                     style="color: #ffffff;"
-                    href="https://api.peacebox.shinbunbun.info/authorize?type=logIn"
-                    >Twitterでログイン</v-btn
-                >
+                    href="https://api.peacebox.sugokunaritai.dev/authorize"
+                >Twitterでログイン</v-btn>
             </v-row>
         </div>
     </div>
 </template>
 
 <script>
-import inbox from '../components/inbox';
+import inbox from '../components/inbox'
+import Cookies from 'js-cookie'
+
 export default {
     data: () => {
-        return { loggedIn: false };
+        return { loggedIn: false }
     },
     mounted() {
-        const param = this.$route.query;
         if (
-            param.userId != undefined &&
-            param.screenName !=
-                undefined /*&&
-            param.accessToken != undefined*/ //TODO:Implement accessToken
+            Cookies.get('userId') != undefined &&
+            Cookies.get('screenName') != undefined
         ) {
-            this.loggedIn = true;
+            this.loggedIn = true
         } else {
-            //Error handler here
+            // Error handler here
+
+            console.log(`False Handler: ${Cookies.get('userId') }  :  ${ Cookies.get('screenName')}`)
         }
     },
     methods: {},
     components: {
         inbox,
     },
-};
+}
 </script>
 
 <style>
