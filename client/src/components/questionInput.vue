0ec7ef4012ebb0cf23aa56dc04a9b69364ef1f75
Yusei Ito <yuseikun@Outlook.jp>
2020-05-10 00:28:52 +0900

    Get userdata from cookie

--- client/src/components/questionInput.vue
+++ client/src/components/questionInput.vue
@@ -4,44 +4,52 @@
             <v-card-actions>
                 <v-list-item class="grow">
                     <v-list-item-avatar color="grey darken-3">
-                        <v-img
-                            class="elevation-6"
-                            :src="questioner.icon"
-                        ></v-img>
+                        <v-img class="elevation-6" :src="questioner.icon"></v-img>
                     </v-list-item-avatar>
                     <v-list-item-content>
-                        <v-list-item-title
-                            >{{ questioner.name }} ({{
-                                questioner.screenName
-                            }})</v-list-item-title
-                        >
+                        <v-list-item-title>
+                            {{ questioner.name }} ({{
+                            questioner.screenName
+                            }})
+                        </v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
             </v-card-actions>
 
             <v-card-text>
-                <v-textarea
-                    success
-                    :placeholder="`${questioner.name}さんに質問しましょう!`"
-                />
+                <v-textarea success :placeholder="`${questioner.name}さんに質問しましょう!`" />
                 <v-btn style="width: 100%;" color="success">送信</v-btn>
             </v-card-text>
         </v-card>
     </div>
 </template>
 <script>
-const questionerDummyObj = {
-    screenName: 'ItyuJ',
-    icon:
-        'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
-    name: 'Yusei Ito',
-}
+
+import Cookies from 'js-cookie'
 
 export default {
     data() {
         return {
-            questioner: questionerDummyObj,
+            questioner: null,
+            loggedIn:false
         }
     },
+    mounted(){
+        if (
+            Cookies.get('userId') != undefined &&
+            Cookies.get('screenName') != undefined
+        ) {
+            this.loggedIn = true
+
+            this.questioner={
+                screenName:Cookies.get('screenName'),
+                icon:Cookies.get('profileImageUrl'),
+                name:Cookies.get('screenName')
+            }
+        } else {
+            
+            
+        }
+    }
 }
 </script>
