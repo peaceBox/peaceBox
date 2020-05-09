7fa7ec827ea5eef90b118c8eb5b1840881290397
Yusei Ito <yuseikun@Outlook.jp>
2020-05-10 00:15:10 +0900

    ユーザページ

--- client/src/pages/_screenName.vue
+++ client/src/pages/_screenName.vue
@@ -1,25 +1,47 @@
 <template>
     <div style="margin-top: 100px;">
-        <v-row v-if="loggedIn" align="center" justify="center">
-            <h1>Hello</h1>
-        </v-row>
-        <v-card>
-            <v-card-title></v-card-title>
-        </v-card>
+        <v-container>
+            <v-card class="header-card">
+                <h1>{{ targetUser.name }} のpeaceBox</h1>
+            </v-card>
+
+            <question-input name="name" />
+        </v-container>
     </div>
 </template>
 
 <script>
+import questionInput from '../components/questionInput'
+
+const targetDummyObj = {
+    screenName: 'shinbunbun_',
+    icon:
+        'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+    name: 'しんぶんぶん',
+}
+
 export default {
-    data: () => {
-        return { loggedIn: false, isRegisterd: true };
+    components: {
+        questionInput,
     },
-};
+    data: () => {
+        return { loggedIn: true, targetUser: targetDummyObj }
+    },
+}
 </script>
 
 <style>
 p {
     size: 20px;
     font-weight: 600;
+}
+
+.header-card {
+    margin-bottom: 20px;
+    padding: 20px;
+}
+
+.header-card h1 {
+    color: #555555;
 }
 </style>
