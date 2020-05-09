cd6d29afd6932b7bcdbe8b9d5f640ce06758aeb7
Yusei Ito <yuseikun@Outlook.jp>
2020-05-10 00:08:58 +0900

    新しい質問の箱を表示

---
+++ client/src/components/questionInput.vue
@@ -0,0 +1,47 @@
+<template>
+    <div style="width: 100%;">
+        <v-card>
+            <v-card-actions>
+                <v-list-item class="grow">
+                    <v-list-item-avatar color="grey darken-3">
+                        <v-img
+                            class="elevation-6"
+                            :src="questioner.icon"
+                        ></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title
+                            >{{ questioner.name }} ({{
+                                questioner.screenName
+                            }})</v-list-item-title
+                        >
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card-actions>
+
+            <v-card-text>
+                <v-textarea
+                    success
+                    :placeholder="`${questioner.name}さんに質問しましょう!`"
+                />
+                <v-btn style="width: 100%;" color="success">送信</v-btn>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script>
+const questionerDummyObj = {
+    screenName: 'ItyuJ',
+    icon:
+        'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+    name: 'Yusei Ito',
+}
+
+export default {
+    data() {
+        return {
+            questioner: questionerDummyObj,
+        }
+    },
+}
+</script>
